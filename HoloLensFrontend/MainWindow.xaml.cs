12a822789f673d8fc3b851bfcb2a65c4ac097f59
Tom Heinzig <heinzigFIAN@outlook.de>
2022-01-13 12:41:45 +0100

    Neuster Stand der Applikation

--- HoloLensFrontend/MainWindow.xaml.cs
+++ HoloLensFrontend/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Net.Http;
+using System.Net;
+using System.IO;
 
 namespace HoloLensFrontend
 {
@@ -22,25 +26,52 @@ namespace HoloLensFrontend
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<string> bauteile;
+        public List<string> url;
+        public TcpClient tcpClient;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            Task.Run(() => Listen());
+           
         }
 
         private void sendButton_Click(object sender, RoutedEventArgs e)
         {
-            TcpClient tcpClient = new TcpClient("localhost", 12021);
-            NetworkStream networkStream = tcpClient.GetStream();
-            string s = "HalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHalloHallo";
-            s += "123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689123456891234568912345689";
-            s += "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-            string input = inputBox.Text;
-            byte[] byteArray = ASCIIEncoding.ASCII.GetBytes(input);
-            networkStream.Write(byteArray);
-            
+            try
+            {
+                //TcpClient tcpClient = new TcpClient(ipBox.Text, Int32.Parse(portBox.Text));
+                NetworkStream networkStream = this.tcpClient.GetStream();
+                string input = inputBox.Text;
+                int index = bauteile.FindIndex(b => b == input);
+                byte[] byteArray = ASCIIEncoding.ASCII.GetBytes(url[index]);
+                networkStream.Write(byteArray);
+                networkStream.Flush();
+                byte[] bytes = new byte[1024];
+                string msg = "";
+                if (networkStream.DataAvailable)
+                {
+                    int count = networkStream.Read(bytes, 0, bytes.Length);
+                    while (count > 0)
+                    {
+                        msg += ASCIIEncoding.ASCII.GetString(bytes, 0, count);
+                        if (count >= bytes.Length)
+                            count = networkStream.Read(bytes, 0, bytes.Length);
+                        else
+                            count = 0;
+                    }
+                    Debug.WriteLine(msg);
+                }
 
+                //HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("http://localhost/ziptest.zip");
+                //myReq.GetResponse().GetResponseStream();
+            }
+
+            catch(SocketException ex)
+            {
+                connectionBox.Header = "Nicht Verbunden";
+                MessageBox.Show(ex.Message, "Verbindungsfehler", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
 
         }
 
@@ -49,36 +80,102 @@ namespace HoloLensFrontend
             TcpListener listener = new TcpListener(12021);
             listener.Start();
 
-            while(true)
+            while (true)
             {
                 TcpClient remoteClient = listener.AcceptTcpClient();
                 NetworkStream ns = remoteClient.GetStream();
                 byte[] bytes = new byte[1024];
-                string msg="";
+                string msg = "";
                 int count = ns.Read(bytes, 0, bytes.Length);
-                while (count>0)
+                while (count > 0)
                 {
-                    msg +=ASCIIEncoding.ASCII.GetString(bytes,0,count);
+                    msg += ASCIIEncoding.ASCII.GetString(bytes, 0, count);
                     if (count >= bytes.Length)
                         count = ns.Read(bytes, 0, bytes.Length);
                     else
                         count = 0;
                 }
                 Debug.WriteLine(msg);
-                Dispatcher.Invoke(() => { messageText.Text = msg; });
-                
-                
             }
 
         }
 
         private void inputBox_Initialized(object sender, EventArgs e)
         {
+            //File.ReadAllText("Bauteile.csv");
+
+            using (var reader = new StreamReader("./Bauteile.csv", Encoding.UTF7))
+            {
+                List<string> bauteile = new List<string>();
+                List<string> url = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(';');
+
+                    bauteile.Add(values[0]);
+                    url.Add(values[1]);
+                    
+                }
+                this.bauteile = bauteile;
+                this.url = url;
+            }
+            inputBox.Text = bauteile[0];
+            
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string bauteil = inputBox.Text;
+            int index = bauteile.FindIndex(b=> b == bauteil);
+            if ((index + 1) < bauteile.Count) inputBox.Text = bauteile[index + 1];
+            else inputBox.Text = bauteile[0];
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string bauteil = inputBox.Text;
+            int index = bauteile.FindIndex(b => b == bauteil);
+            if (index == 0) inputBox.Text = bauteile[bauteile.Count - 1];
+            else inputBox.Text = bauteile[index - 1];
+        }
+
+        private void connectionBox_Initialized(object sender, EventArgs e)
+        {
+            Task.Run(() => Listen());
+            ipBox.Text = "172.22.100.147";
+            portBox.Text = "12021";
+            try
+            {
+                int port = Int32.Parse(portBox.Text);
+                this.tcpClient = new TcpClient(ipBox.Text, port);
+                connectionBox.Header = "Verbunden";
+            }
+
+            catch (SocketException ex)
+            {
+                connectionBox.Header = "Nicht Verbunden";
+                MessageBox.Show(ex.Message, "Verbindungsfehler", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
 
         }
 
-        private void messageText_Initialized(object sender, EventArgs e)
+        private void connectionButton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                int port = Int32.Parse(portBox.Text);
+                this.tcpClient = new TcpClient(ipBox.Text, port);
+                connectionBox.Header = "Verbunden";
+            }
+
+            catch (SocketException ex)
+            {
+                connectionBox.Header = "Nicht Verbunden";
+                MessageBox.Show(ex.Message, "Verbindungsfehler", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
 
         }
     }
